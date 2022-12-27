dc8f9c537cd0daef37f005215121efa5481c4185
netanel <netanelha@post.jce.ac.il>
2022-12-27 14:20:13 +0200

    update server url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://code-x.onrender.com', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
